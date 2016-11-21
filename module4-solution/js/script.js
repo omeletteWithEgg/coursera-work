c5ae34eacbc5d01fd6baf037cb9478704569807f
unknown <330611730@qq.com>
2016-11-21 10:48:19 +0800

    modified

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -66,8 +66,8 @@ for (var i = 0;i<names.length;i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter=="j") {
-    console.log(byeSpeaker.speak(names[i]));
+    byeSpeaker.speak(names[i]);
   } else {
-    console.log(helloSpeaker.speak(names[i]));
+    helloSpeaker.speak(names[i]);
   }
 }
